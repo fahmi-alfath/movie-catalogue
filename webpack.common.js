ea44f2e654a27e828fe497a5dd14d8169190038b
Fahmi Alfath <fahmi243alfath@gmail.com>
2023-07-15 06:57:50 +0700

    load dotenv

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
